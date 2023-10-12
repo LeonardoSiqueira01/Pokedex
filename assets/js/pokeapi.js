8ab4b4fb31e598b502855b939f5f90093f20ef24
Leonardo Siqueira <Leonardo.gabriel01@hotmail.com>
2023-10-12 10:58:27 -0300

    .

--- assets/js/pokeapi.js
+++ assets/js/pokeapi.js
@@ -8,3 +8,15 @@ pokeApi.getPokemons = (offset = 0, limit = 10) =>{
 .then((JsonBody) => JsonBody.results)
 .catch((error) => console.error(error));
 }
+
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5'),
+    fetch('https://pokeapi.co/api/v2/pokemon/6')
+]).then((results) => {
+    console.log(results)
+})
